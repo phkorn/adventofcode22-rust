60d2b7d41dec03c03b1b1d9173a2fa5022c3d4c4
Philipp Korn <korn.philipp@gmail.com>
2023-10-13 19:39:08 +0200

    add 02 second remove result wrapper from mappers simplify output

--- src/bin/02/main.rs
+++ src/bin/02/main.rs
@@ -4,6 +4,8 @@ use adventofcode22::read_lines_by_line;
 const ROCK: isize = 1;
 const PAPER:  isize = 2;
 const SCISSOR:  isize = 3;
+
+#[derive(Debug, Clone, Copy)]
 enum RPSMove {
     Rock = ROCK,
     Paper = PAPER,
@@ -33,67 +35,152 @@ enum EnemyMove {
 
 
 struct Move {
-    enemy: isize,
-    player: isize,
+    enemy: RPSMove,
+    player: RPSMove,
 }
 
-fn to_cryptic_player_move(txt: &str) -> Result<CrypticPlayerMove> {
+fn to_cryptic_player_move(txt: &str) -> CrypticPlayerMove {
     match txt {
-        "X" => Ok(CrypticPlayerMove::X),
-        "Y" => Ok(CrypticPlayerMove::Y),
-        "Z" => Ok(CrypticPlayerMove::Z),
+        "X" => CrypticPlayerMove::X,
+        "Y" => CrypticPlayerMove::Y,
+        "Z" => CrypticPlayerMove::Z,
         _ => panic!("unhandled cryptic player move")
     }
 }
 
-fn to_enemy_move(txt: &str) -> Result<EnemyMove> {
+fn to_enemy_move(txt: &str) -> EnemyMove {
     match txt {
-        "A" => Ok(EnemyMove::A),
-        "B" => Ok(EnemyMove::B),
-        "C" => Ok(EnemyMove::C),
+        "A" => EnemyMove::A,
+        "B" => EnemyMove::B,
+        "C" => EnemyMove::C,
         _ => panic!("unhandled enemy move")
     }
 }
 
-fn to_result(m: Move) -> Result<RPSResult> {
-    match m {
-        Move {enemy: ROCK, player: ROCK } => Ok(RPSResult::Draw),
-        Move {enemy: ROCK, player: PAPER } => Ok(RPSResult::Won),
-        Move {enemy: ROCK, player: SCISSOR } => Ok(RPSResult::Lost),
-        Move {enemy: PAPER, player: PAPER } => Ok(RPSResult::Draw),
-        Move {enemy: PAPER, player: SCISSOR } => Ok(RPSResult::Won),
-        Move {enemy: PAPER, player: ROCK } => Ok(RPSResult::Lost),
-        Move {enemy: SCISSOR, player: SCISSOR } => Ok(RPSResult::Draw),
-        Move {enemy: SCISSOR, player: ROCK } => Ok(RPSResult::Won),
-        Move {enemy: SCISSOR, player: PAPER } => Ok(RPSResult::Lost),
-        _ => panic!("unhandled move to result")
+fn to_suggested_result(txt: &str) -> RPSResult {
+    match txt {
+        "X" => RPSResult::Lost,
+        "Y" =>RPSResult::Draw,
+        "Z" => RPSResult::Won,
+        _ => panic!("unhandled intended result")
     }
 }
-fn main() ->  Result<()> {
+
+fn enemy_move_to_rps_move(enemy_move: &EnemyMove) -> RPSMove {
+    match enemy_move {
+        EnemyMove::A => RPSMove::Rock,
+        EnemyMove::B => RPSMove::Paper,
+        EnemyMove::C => RPSMove::Scissor
+    }
+}
+
+fn cryptic_player_move_to_rps_move(enemy_move: &CrypticPlayerMove) -> RPSMove {
+    match enemy_move {
+        CrypticPlayerMove::X => RPSMove::Rock,
+        CrypticPlayerMove::Y => RPSMove::Paper,
+        CrypticPlayerMove::Z => RPSMove::Scissor
+    }
+}
+
+
+fn suggested_rps_move(hint: (&RPSMove, &RPSResult)) -> RPSMove {
+    match hint {
+        (RPSMove::Rock, RPSResult::Won) => RPSMove::Paper,
+        (RPSMove::Rock, RPSResult::Lost) => RPSMove::Scissor,
+        (RPSMove::Rock, RPSResult::Draw) => RPSMove::Rock,
+        (RPSMove::Paper, RPSResult::Won) => RPSMove::Scissor,
+        (RPSMove::Paper, RPSResult::Lost) => RPSMove::Rock,
+        (RPSMove::Paper, RPSResult::Draw) => RPSMove::Paper,
+        (RPSMove::Scissor, RPSResult::Won) => RPSMove::Rock,
+        (RPSMove::Scissor, RPSResult::Lost) => RPSMove::Paper,
+        (RPSMove::Scissor, RPSResult::Draw) => RPSMove::Scissor,
+    }
+}
+
+fn to_result(m: Move) -> RPSResult {
+    match m {
+        Move {enemy: RPSMove::Rock, player: RPSMove::Rock } => RPSResult::Draw,
+        Move {enemy: RPSMove::Rock, player: RPSMove::Paper } => RPSResult::Won,
+        Move {enemy: RPSMove::Rock, player: RPSMove::Scissor } => RPSResult::Lost,
+        Move {enemy: RPSMove::Paper, player: RPSMove::Paper } => RPSResult::Draw,
+        Move {enemy: RPSMove::Paper, player: RPSMove::Scissor } => RPSResult::Won,
+        Move {enemy: RPSMove::Paper, player: RPSMove::Rock } => RPSResult::Lost,
+        Move {enemy: RPSMove::Scissor, player: RPSMove::Scissor } => RPSResult::Draw,
+        Move {enemy: RPSMove::Scissor, player: RPSMove::Rock } => RPSResult::Won,
+        Move {enemy: RPSMove::Scissor, player: RPSMove::Paper } => RPSResult::Lost,
+    }
+}
+
+
+fn first() -> Result<()> {
     let path = "src/bin/02/input.txt";
     let mut sum = 0;
 
     read_lines_by_line(path, |line|{
         let split: Vec<&str> = line.split_whitespace().collect();
 
-        let enemy_move_letter = to_enemy_move(split.first().unwrap())?;
-        let enemy_move = enemy_move_letter as isize;
+        let enemy_move_letter = to_enemy_move(split.first().unwrap());
+        let enemy_move = enemy_move_to_rps_move(&enemy_move_letter);
 
-        let cryptic_player_move_letter = to_cryptic_player_move(split.last().unwrap())?;
-        let cryptic_player_move = cryptic_player_move_letter as isize;
+        let cryptic_player_move_letter = to_cryptic_player_move(split.last().unwrap());
+        let player_move = cryptic_player_move_to_rps_move(&cryptic_player_move_letter);
+        let player_move_num =  player_move as isize;
 
-        let m: Move = Move { enemy:enemy_move, player: cryptic_player_move };
+        // move is a keyword
+        let mv: Move = Move { enemy:enemy_move, player: player_move };
 
-        let result = to_result(m)?;
+        let result = to_result(mv);
         let result_game = result as isize;
-        let result_round = result_game + cryptic_player_move;
+        let result_round = result_game + player_move_num;
 
-        println!("enemyL {:?} enemyV {:?}, playerL {:?} playerV {:?}, result {:?} resultG {:?} resultR {:?} sum {}", enemy_move_letter,enemy_move, cryptic_player_move_letter, cryptic_player_move, result, result_game, result_round, sum);
+        println!("{:?} ⚔️ {:?} -> {:?} | #game {:?}, #round {:?}, sum {}", player_move, enemy_move, result, result_game, result_round, sum);
         sum += result_round;
         Ok(())
     })?;
 
-    println!("\nsum: {}", sum);
+    println!("\nTotal Result: {}", sum);
 
+    Ok(())
+} 
+
+fn second() -> Result<()> {
+    let path = "src/bin/02/input.txt";
+    let mut sum = 0;
+
+    read_lines_by_line(path, |line|{
+        let split: Vec<&str> = line.split_whitespace().collect();
+
+        let enemy_move_cryptic = to_enemy_move(split.first().unwrap());
+        let enemy_move = enemy_move_to_rps_move(&enemy_move_cryptic);
+
+        let intended_result_letter = to_suggested_result(split.last().unwrap());
+
+        let player_move = suggested_rps_move((&enemy_move, &intended_result_letter));
+        let player_move_num =  player_move as isize;
+        
+        let mv: Move = Move { enemy:enemy_move, player: player_move };
+
+        let result = to_result(mv);
+        let result_game = result as isize;
+        let result_round = result_game + player_move_num;
+
+        println!("{:?} ⚔️ {:?} -> {:?} | #game {:?}, #round {:?}, sum {}", player_move, enemy_move, result, result_game, result_round, sum);
+        sum += result_round;
+        Ok(())
+    })?;
+
+    println!("\nTotal Result: {}", sum);
+
+    Ok(())
+} 
+
+fn main() ->  Result<()> {
+    println!("##### first ####");
+    // 12772
+    first()?;
+
+    println!("##### second ####");
+    // 11618
+    second()?;
     Ok(())
 }
