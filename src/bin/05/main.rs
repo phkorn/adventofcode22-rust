6a4f4d4dc0ab02092bb4e5c61e12a9c3f619200c
Philipp Korn <korn.philipp@gmail.com>
2023-12-01 12:27:43 +0100

    add 05

--- src/bin/05/main.rs
+++ src/bin/05/main.rs
@@ -1,7 +1,7 @@
 use adventofcode22::read_lines_by_line;
 use itertools::Itertools;
+use regex::Regex;
 use std::{io::Result, ops::Range};
-use regex::{Regex};
 
 #[derive(Debug)]
 struct Move {
@@ -10,69 +10,100 @@ struct Move {
     to: usize,
 }
 
-fn print_vectors(vectors: &Vec<Vec<Option<char>>>){
+type Stacks = Vec<Vec<Option<char>>>;
+
+fn print_vectors(vectors: &Stacks) {
     for (idx, v) in vectors.iter().enumerate() {
-        print!("{}: ", idx+1);
+        print!("{}: ", idx + 1);
         for e in v {
-           print!("{} ", e.unwrap()) 
+            print!("{} ", e.unwrap())
         }
         println!()
     }
+    println!()
 }
 
-fn add_to_vecs(vectors: &mut Vec<Vec<Option<char>>>, idx: usize, cargo: Option<char>){
+fn add_to_vecs(vectors: &mut Stacks, idx: usize, cargo: Option<char>) {
     if vectors.get(idx).is_none() {
         vectors.insert(idx, Vec::new());
     }
     vectors[idx].push(cargo);
 }
 
-fn handle_move(vectors: &mut Vec<Vec<Option<char>>>, mov: Move) {
+fn handle_move(vectors: &mut Stacks, mov: &Move) {
     println!("{:?}", mov);
     let slice: Vec<Option<char>>;
     {
-        let v_from: &mut Vec<Option<char>> = vectors[mov.from-1].as_mut();
-        let range = Range{ start: v_from.len() - mov.quantity, end: v_from.len()};
+        let v_from: &mut Vec<Option<char>> = vectors[mov.from - 1].as_mut();
+        let range = Range {
+            start: v_from.len() - mov.quantity,
+            end: v_from.len(),
+        };
         let x = v_from.drain(range);
         slice = x.collect_vec();
     }
-    let v_to: &mut Vec<Option<char>> = vectors[mov.to-1].as_mut();
+    let v_to: &mut Vec<Option<char>> = vectors[mov.to - 1].as_mut();
     slice.iter().rev().for_each(|elem| {
-        v_to.push(elem.clone());
+        v_to.push(*elem);
     });
 }
 
-fn first() -> Result<()> {
+fn handle_move_multiple(vectors: &mut Stacks, mov: &Move) {
+    println!("{:?}", mov);
+    let slice: Vec<Option<char>>;
+    {
+        let v_from: &mut Vec<Option<char>> = vectors[mov.from - 1].as_mut();
+        let range = Range {
+            start: v_from.len() - mov.quantity,
+            end: v_from.len(),
+        };
+        let x = v_from.drain(range);
+        slice = x.collect_vec();
+    }
+    let v_to: &mut Vec<Option<char>> = vectors[mov.to - 1].as_mut();
+    slice.iter().for_each(|elem| {
+        v_to.push(*elem);
+    });
+}
+
+fn move_cargo(mut move_fn: impl FnMut(&mut Stacks, &Move) -> ()) -> Result<()> {
     let path = "src/bin/05/input.txt";
     let regex_stack = Regex::new(r"(?m)\s?\[([A-Z])\]\s?|[^\d]\s(\s)\s").unwrap();
     let regex_moves = Regex::new(r"(?m)move (\d+) from (\d+) to (\d+)").unwrap();
 
-    let mut vectors: Vec<Vec<Option<char>>> = Vec::new();
+    let mut vectors: Stacks = Vec::new();
     let mut moves: Vec<Move> = Vec::new();
 
     read_lines_by_line(path, |line| {
         match line {
-         x if regex_stack.is_match(x) => {
-            let iter = regex_stack.captures_iter(x);
-            for (idx, mat) in iter.enumerate() {
-                println!("{:?}", mat);
-                match (mat.get(1), mat.get(2)) {
-                    (Some(cargo), None) => add_to_vecs(&mut vectors, idx, cargo.as_str().chars().next()),
-                    (None, Some(_)) => add_to_vecs(&mut vectors, idx, None),
-                    _ => panic!("missing match clause in stack")
+            x if regex_stack.is_match(x) => {
+                let iter = regex_stack.captures_iter(x);
+                for (idx, mat) in iter.enumerate() {
+                    println!("{:?}", mat);
+                    match (mat.get(1), mat.get(2)) {
+                        (Some(cargo), None) => {
+                            add_to_vecs(&mut vectors, idx, cargo.as_str().chars().next())
+                        }
+                        (None, Some(_)) => add_to_vecs(&mut vectors, idx, None),
+                        _ => panic!("missing match clause in stack"),
+                    }
                 }
-            };
-         },
-         x if regex_moves.is_match(x) => {
-            let iter = regex_moves.captures_iter(x);
-            for mov in iter {
-                moves.push(Move { quantity: mov.get(1).unwrap().as_str().parse::<usize>().unwrap(), from: mov.get(2).unwrap().as_str().parse::<usize>().unwrap(), to: mov.get(3).unwrap().as_str().parse::<usize>().unwrap() });     
             }
-         },
-         _ => println!("<skipped>")
+            x if regex_moves.is_match(x) => {
+                let iter = regex_moves.captures_iter(x);
+                for mov in iter {
+                    moves.push(Move {
+                        quantity: mov.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+                        from: mov.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+                        to: mov.get(3).unwrap().as_str().parse::<usize>().unwrap(),
+                    });
+                }
+            }
+            _ => println!("<skipped>"),
         }
         Ok(())
     })?;
+
     for vec in vectors.iter_mut() {
         vec.reverse();
         vec.retain(|&x| x.is_some());
@@ -80,35 +111,37 @@ fn first() -> Result<()> {
 
     print_vectors(&vectors);
     for mov in moves {
-        handle_move(&mut vectors, mov);
+        move_fn(&mut vectors, &mov);
         print_vectors(&vectors);
-        println!();
     }
 
     for v in vectors {
         print!("{}", v.last().unwrap().unwrap());
     }
     println!();
+
+    Ok(())
+}
+
+fn first() -> Result<()> {
+    move_cargo(handle_move)?;
     Ok(())
 }
 
 fn second() -> Result<()> {
-    let path = "src/bin/XX/input.txt";
-    let mut sum = 0;
-
-    read_lines_by_line(path, |line| {
-        Ok(())
-    })?;
+    move_cargo(handle_move_multiple)?;
     Ok(())
 }
 
+
+
 fn main() -> Result<()> {
     println!("##### first ####");
-    //Result
+    //Result WHTLRMZRC
     first()?;
 
     println!("##### second ####");
-    //Result
+    //Result GMPMLWNMG
     second()?;
     Ok(())
 }
